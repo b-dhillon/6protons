733d01d62b609bac0ea84d679b09319925b33240
Bhav-Dhillon <b.dhillon913@gmail.com>
2022-07-26 23:36:04 -0700

    minor phrasing change in lesson loading screen

--- src/pages/fullerenes_lesson/FullerenesLesson.jsx
+++ src/pages/fullerenes_lesson/FullerenesLesson.jsx
@@ -31,7 +31,7 @@ function FullerenesLesson(props) {
               <div></div>
               <div></div>
             </div>
-            <h1>Loading Fullerenes.</h1>
+            <h1>Loading lesson.</h1>
           </div>
         </>
       )
