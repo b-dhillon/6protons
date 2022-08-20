25bcd4297bd65a04f54275c52b4c0dc4c7428496
Bhav-Dhillon <b.dhillon913@gmail.com>
2022-08-19 18:56:46 -0700

    add diamond model and animation

--- src/pages/diamonds_lesson/Models.jsx
+++ src/pages/diamonds_lesson/Models.jsx
@@ -2,6 +2,8 @@ import React, { useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useFrame, } from '@react-three/fiber'
 import { useGLTF, useAnimations } from '@react-three/drei';
+import * as THREE from 'three';
+
 
 useGLTF.preload('/lesson2_models/model0.glb');
 
@@ -19,19 +21,37 @@ function LessonModels() {
       const { actions } = useAnimations(animations, group);
 
       const ref = useRef();
-      function ScaleRotateAnimation() {
+
+      function RotateAnimation() {
         useFrame((state) => {
           ref.current.rotation.y += 0.002
         })
       }
 
+      useEffect(() =>
+      {
+        if(counter >= 0)
+        {
+          console.log(actions);
+          const _animations = Object.values(actions);
+          _animations.forEach((a) => a.play())
+  
+        }
+      })
+
       if(counter === 0)
       {
-        ScaleRotateAnimation();
+        // RotateAnimation();
         return (
-          <group ref={ref} {...props} dispose={null} position={[0, -0.5, -1]} scale={0.5}>
-            <mesh geometry={nodes.Cube001.geometry} material={materials.Material} />
-            <mesh geometry={nodes.Cube001_1.geometry} material={materials['S Material']} />
+          <group ref={group} {...props} dispose={null} position={[0, -0.5, -1]} scale={0.5}>
+            <group name="Scene">
+              <group name="Empty" scale={0.1}>
+                <group name="ZnS_Unit_Cell" rotation={[0, -0.83, 0]}>
+                  <mesh name="Cube001" geometry={nodes.Cube001.geometry} material={materials.Material} />
+                  <mesh name="Cube001_1" geometry={nodes.Cube001_1.geometry} material={materials['S Material']} />
+                </group>
+              </group>
+            </group>
           </group>
         )
       } 
--- todo.md
+++ todo.md
@@ -1,5 +1,14 @@
 ## To-do
+    - Create scale animation for diamond model 
+    - Add some copy for section 1 of diamond lesson
+
+
     - Add nanotube model for start lesson page
     - Make home page overlay responsive
-    - Add home button to other lessons
-    - Add back to lesson selection page
+    - Add steroid hormone label on model flip of home page
+    - Change home button design -- either go make to circle expansion or something else 
+    
+
+
+
+    - Add back to lesson selection page?
