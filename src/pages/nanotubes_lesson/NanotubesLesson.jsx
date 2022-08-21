31f62f946eea73159b6778b777e4d5b2d39d461b
Bhav-Dhillon <b.dhillon913@gmail.com>
2022-08-21 13:02:10 -0700

    add nanotubes under constructiob

--- src/pages/nanotubes_lesson/NanotubesLesson.jsx
+++ src/pages/nanotubes_lesson/NanotubesLesson.jsx
@@ -1,10 +1,15 @@
 import { Suspense} from 'react'
 import { Canvas } from '@react-three/fiber'
 import Stars from '../../components/Stars'
+import HomeNav from '../../components/HomeNav';
 
-function NanotubesLesson() {
+
+function NanotubesLesson(props) {
     return (
         <>
+            <h1 style={{position: 'absolute', zIndex: 3, top: '40%', left: '24%'}}>This part of the world is under construction.</h1>
+
+            <HomeNav setPage={props.setPage} />
             <Canvas gl={{alpha: false}} dpr={[1, 2]} camera={{ near: 0.01, far: 10, fov: 75, position: [0,0,5] }}>
                 <color attach="background" args={["#000000"]} />
                 <Suspense fallback={null}>
