31dc285a9b62296509554252ccb884da6d9ae966
Bhav-Dhillon <b.dhillon913@gmail.com>
2022-08-27 21:42:18 -0700

    test

--- src/pages/fullerenes_lesson/FullerenesLesson.jsx
+++ src/pages/fullerenes_lesson/FullerenesLesson.jsx
@@ -13,7 +13,6 @@ import './fullerene-styles.css'
 function FullerenesLesson(props) {
     const [lessonLoading, setLessonLoading] = useState(true)
 
-    // const spinner = document.querySelector(".spinnerWrapper");
 
     useEffect(() =>
     {
@@ -21,36 +20,20 @@ function FullerenesLesson(props) {
     }, [])
 
     // useEffect(() => {
-    //   if (lessonLoading)
+    //   if (!lessonLoading)
     //   {
-    //     const lessonSelection = document.querySelector(".lessonSelection--wrapper");
-    //     lessonSelection.classList.remove("hidden")
-    //     const hero = document.querySelector(".hero--wrapper");
-    //     hero.classList.add("hidden")
-  
-  
-    //     console.log('done');
-    //   } 
-    //   else {
-    //     const lessonSelection = document.querySelector(".lessonSelection--wrapper");
-    //     if(lessonSelection) {
-    //       lessonSelection.classList.add("hidden")
-    //       const hero = document.querySelector(".hero--wrapper");
-    //       hero.classList.remove("hidden")
-    //       console.log('done');
-  
-    //     }
-  
+    //     const spinner = document.querySelector(".spinnerWrapper");
+    //     spinner.classList.add("hidden")
     //   }
     // }, [lessonLoading])
 
 
   
   
-    // if (lessonLoading) {
+    if (lessonLoading) {
       return (
         <>
-          {/* <div className='spinnerWrapper'>
+          <div className='spinnerWrapper'>
             <div className='spinner'>
               <div></div>
               <div></div>
@@ -61,10 +44,9 @@ function FullerenesLesson(props) {
             </div>
             <h1 style={{marginTop: 80}}>loading fullerenes</h1>
           </div>
-           */}
+          
 
-          <Canvas gl={{alpha: false}} dpr={[1, 2]} camera={{ near: 0.01, far: 10, fov: 45, position: [0, 0, 2] }}>
-              {/* <color attach="background" args={["#000000"]} /> */}
+          {/* <Canvas gl={{alpha: false}} dpr={[1, 2]} camera={{ near: 0.01, far: 10, fov: 45, position: [0, 0, 2] }}>
               <Suspense fallback={null}>
                 <spotLight position={[10, 10, 10] } intensity={.8}/>
                 <ambientLight intensity={.3} />
@@ -76,33 +58,32 @@ function FullerenesLesson(props) {
           </Canvas>
           <LessonNav />
           <HomeNav setPage={props.setPage} setOverlay={props.setOverlay} />
-          <FullereneText />
-          
-
-
+          <FullereneText /> */}
+        
         </>
       )
-    // }
+    }
 
-    // else return (
-    //     <>
-    //         {/* <Stats showPanel={0} className="stats" {...props} /> */}
-    //         <HomeNav setPage={props.setPage} setOverlay={props.setOverlay} />
-    //         <LessonNav />
-    //         <FullereneText />
-    //         <Canvas gl={{alpha: false}} dpr={[1, 2]} camera={{ near: 0.01, far: 10, fov: 45, position: [0, 0, 2] }}>
-    //             <color attach="background" args={["#000000"]} />
-    //             <Suspense fallback={null}>
-    //               <spotLight position={[10, 10, 10] } intensity={.8}/>
-    //               <ambientLight intensity={.3} />
-    //               <MemoizedStars />
-    //               <Provider store={DataStore}>
-    //                   <FullereneModels/>
-    //               </Provider>
-    //             </Suspense>
-    //         </Canvas>
-    //     </>
-    // )
+    else return (
+        <>
+            {/* <Stats showPanel={0} className="stats" {...props} /> */}
+
+            <Canvas gl={{alpha: false}} dpr={[1, 2]} camera={{ near: 0.01, far: 10, fov: 45, position: [0, 0, 2] }}>
+                <color attach="background" args={["#000000"]} />
+                <Suspense fallback={null}>
+                  <MemoizedStars />
+                  <Provider store={DataStore}>
+                      <FullereneModels/>
+                  </Provider>
+                  <spotLight position={[10, 10, 10] } intensity={.8}/>
+                  <ambientLight intensity={.3} />
+                </Suspense>
+            </Canvas>
+            <HomeNav setPage={props.setPage} setOverlay={props.setOverlay} />
+            <LessonNav />
+            <FullereneText />
+        </>
+    )
 }
 
 export default FullerenesLesson;
