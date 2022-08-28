1209d2c5d8ffbf51bcfc04119f1e68818801849d
Bhav-Dhillon <b.dhillon913@gmail.com>
2022-08-27 21:21:00 -0700

    take out background of canvas -- not needed

--- src/pages/fullerenes_lesson/FullerenesLesson.jsx
+++ src/pages/fullerenes_lesson/FullerenesLesson.jsx
@@ -64,7 +64,7 @@ function FullerenesLesson(props) {
            */}
 
           <Canvas gl={{alpha: false}} dpr={[1, 2]} camera={{ near: 0.01, far: 10, fov: 45, position: [0, 0, 2] }}>
-              <color attach="background" args={["#000000"]} />
+              {/* <color attach="background" args={["#000000"]} /> */}
               <Suspense fallback={null}>
                 <spotLight position={[10, 10, 10] } intensity={.8}/>
                 <ambientLight intensity={.3} />
