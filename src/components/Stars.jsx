29172ceb0efdaa813509ef4f89d4c863f9a388e0
Bhav-Dhillon <b.dhillon913@gmail.com>
2022-09-25 15:06:51 -0700

    make stars bigger + fix typo + add border radius

--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -22,7 +22,7 @@ const Stars = function Stars(props) {
         <>
             <group rotation={[0, 0, Math.PI / 4]}>
                 <Points ref={ref} positions={sphere} stride={3} frustumCulled={false} {...props}>
-                    <PointMaterial transparent color="#fff" size={0.003} sizeAttenuation={true} depthWrite={false} />
+                    <PointMaterial transparent color="#fff" size={0.0035} sizeAttenuation={true} depthWrite={false} />
                 </Points>
             </group>
         </>
--- src/pages/nanotubes_lesson/NanotubeText.jsx
+++ src/pages/nanotubes_lesson/NanotubeText.jsx
@@ -46,8 +46,7 @@ function NanotubeText(props){
     {
         return(
             <div className='text--wrapper' style={{flexDirection: 'column'}}>
-                <p>This lesson is being developed. Please see the lesson on <span>Fullerenes</span>
-                 for an example of the minimal viable product (MVP) of this project.
+                <p>This lesson is being developed. Please see the lesson on <span>Fullerenes</span> for an example of the minimal viable product (MVP) of this project.
                 </p>
 
                 <button className='fullerene-redirect' onClick={() => {
