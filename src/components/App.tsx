1751b74a2244dd458f45ec7d7eeeed300dc60eb5
B. Dhillon <88413313+b-dhillon@users.noreply.github.com>
2023-01-05 20:05:27 -0800

    Create AddAllMeshesOfAppToData fxn

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,9 @@
+import { useState } from 'react';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 
-export default function App() {
-
-
-    return <p>Error</p>;
-};
-
-const pages = {
-    "test_page": {
+const _pages = [
+    {
         id: 'test_page',
         title: 'Fullerenes',
         thumbnail: "url('./lesson_thumbnails/fullereneTile.png')",
@@ -90,28 +85,93 @@ const pages = {
         speach: null,
         music: null
     },
-};
+    {
+        id: 'nanotube',
+        title: 'Nanotubes',
+        thumbnail: "url('./lesson_thumbnails/nanotube.png')",
+        speach: null,
+        text: null,
+        models: [],
+    },
+
+    {
+        id: 'diamond',
+        title: 'Diamonds',
+        thumbnail: "url('./lesson_thumbnails/diamond.png')",
+        speach: null,
+        text: null,
+        models: [
+            {
+                id: "model0", 
+                path: '/lesson3_models/model0.glb', 
+                meshes: null
+            }
+        ],
+    },
+
+    {
+        id: 'graphenes',
+        title: 'Graphenes',
+        thumbnail: "url('./lesson_thumbnails/graphene.png')",
+        speach: null,
+        text: null,
+        models: [
+            {
+                id: "model0", 
+                path: '/lesson3_models/model0.glb', 
+                meshes: null
+            }
+        ],
+    },
+
+    {
+        id: 'chirality',
+        title: 'Chirality',
+        thumbnail: "url('./lesson_thumbnails/chirality.png')",
+        speach: null,
+        text: null,
+        models: [
+            {
+                id: "model0", 
+                path: '/lesson3_models/model0.glb', 
+                meshes: null
+            }
+        ],
+    }
+];
+
+// const modelPaths = [ [ '/lesson3_models/model0.glb', '/lesson4_models/model1.glb' ], ['/lesson3_models/model0.glb', '/lesson4_models/model1.glb'], ['/lesson3_models/model0.glb', '/lesson4_models/model1.glb'], ['/lesson3_models/model0.glb', '/lesson4_models/model1.glb'] ]
 
 
-async function AddAllMeshesToPageDataOf( page: keyof typeof pages ) {
-    const extracted_meshes = await ExtractAllMeshesOf( page );
-    const new_pages = JSON.parse(JSON.stringify(pages));
-    new_pages.test_page.models = new_pages.test_page.models.map( (model: any, i: number) => {
-        return { ...model, meshes: extracted_meshes[i] }
-    });
-    console.log(new_pages);
-    return new_pages;
+export default function App() {
 
-    async function LoadAllModelsOf( page: keyof typeof pages ) {
+    const [ pages , setPages ] = useState( _pages );
 
-        function LoadModel( i: number ) {
+    async function AddAllMeshesOfAppToData() {
+        const allMeshesOfApp = await ExtractAllMeshesOfApp(); 
+    
+        setPages( oldPages  => {
+            return oldPages.map( (oldPage: any, i: number) => {
+                return {
+                    ...oldPage, 
+                    models: oldPage.models.map( ( model: any, j: number ) => {
+                        return {
+                            ...model, 
+                            meshes: allMeshesOfApp[i][j]
+                        }
+                    })
+                }
+            }) 
+        });
+    
+        function LoadModel( path: any ) {
             return new Promise( (resolve, reject) => {
                 const loader = new GLTFLoader();
                 const dracoLoader = new DRACOLoader();
                 dracoLoader.setDecoderPath( 'https://www.gstatic.com/draco/v1/decoders/' );
                 loader.setDRACOLoader( dracoLoader );
                 loader.load(
-                    pages[page].models[i].path,
+                    path,
                     (gltf: any) => {
                         resolve(gltf);
                         console.log('loaded');
@@ -126,20 +186,157 @@ async function AddAllMeshesToPageDataOf( page: keyof typeof pages ) {
                 );
             });
         };
-    
-        const models: any = [];
-        for (let i = 0; i < pages[page].models.length; i++) {
-            models[i] = await LoadModel(i);
+        
+        async function LoadAllModelsOfApp() {
+            const allModelsOfApp: any = [] // [ [ model0, model1, model2 ], [ model0, model1, model2 ], [ model0, model1, model2  ] ]
+                                            //         ^ models[] of page0             ^models[] of page1               ^models[] of page2
+        
+            _pages.forEach( async (page: any ) => {
+                const models: any = []; // [ model0, model1, model2 ]
+        
+                for( let i = 0; i < page.models.length; i++) {
+                    models[i] = await LoadModel(page.models[i].path);
+                }
+                allModelsOfApp.push(models)
+            })
+            return allModelsOfApp; 
+        }
+        
+        async function ExtractAllMeshesOfApp() {
+            const allModelsOfApp = await LoadAllModelsOfApp();
+        
+            const allMeshesOfApp = allModelsOfApp.map( (modelsArr: any) => {
+                modelsArr.map( (gltf: any) => gltf.scene.children.filter( ( child: any ) => child.isMesh && child.__removed === undefined ) );
+            } ) // [ [[Mesh], [Mesh], [Mesh]], [[Mesh], [Mesh], [Mesh]], [[Mesh], [Mesh],  [Mesh]]
+        
+            return allMeshesOfApp; 
         };
-        return models; // [model0, model1, model2, ...]
     };
-    
-    async function ExtractAllMeshesOf( page: keyof typeof pages ) {
-        const models = await LoadAllModelsOf( page );
-        const extracted_meshes = models.map( (gltf: any) => gltf.scene.children.filter( ( child: any ) => child.isMesh && child.__removed === undefined ));
-        return extracted_meshes; // [ [mesh0, mesh1, mesh2, ...], [mesh0, mesh1, mesh2, ...], [mesh0, mesh1, mesh2, ...], ... ]
-    }
+
+    return <p>Error</p>;
 };
 
-AddAllMeshesToPageDataOf( 'test_page' );
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+    async function AddAllMeshesToPageDataOf( page: keyof typeof pages ) {
+        const extracted_meshes = await ExtractAllMeshesOf( page );
+
+
+        setPages( ( oldPages: any ) => {
+            return oldPages.map( (oldPage: any) => {
+                oldPage === page ? oldPage.models.map( (model: any, i: number) => { 
+                    return { ...model, meshes: extracted_meshes[i] }
+                }) : oldPage;
+            });
+        });
+    
+        
+        // const new_pages = JSON.parse(JSON.stringify(_pages));
+        // new_pages.test_page.models = new_pages.test_page.models.map( (model: any, i: number) => {
+        //     return { ...model, meshes: extracted_meshes[i] }
+        // });
+        // console.log(new_pages);
+        // return new_pages;
+    
+        async function LoadAllModelsOf( page: keyof typeof pages ) {
+    
+            function LoadModel( i: number ) {
+                return new Promise( (resolve, reject) => {
+                    const loader = new GLTFLoader();
+                    const dracoLoader = new DRACOLoader();
+                    dracoLoader.setDecoderPath( 'https://www.gstatic.com/draco/v1/decoders/' );
+                    loader.setDRACOLoader( dracoLoader );
+                    loader.load(
+                        pages[page].models[i].path,
+                        (gltf: any) => {
+                            resolve(gltf);
+                            console.log('loaded');
+                        },
+                        (xhr: any) => {
+                            // console.log((xhr.loaded / xhr.total) + 'loaded');
+                        },
+                        (error: any) => {
+                            console.error(error);
+                            reject(error);
+                        }
+                    );
+                });
+            };
+        
+            const models: any = [];
+            for (let i = 0; i < pages[page].models.length; i++) {
+                models[i] = await LoadModel(i);
+            };
+            return models; // [model0, model1, model2, ...]
+        };
+        
+        async function ExtractAllMeshesOf( page: keyof typeof pages ) {
+            const models = await LoadAllModelsOf( page );
+            const extracted_meshes = models.map( (gltf: any) => gltf.scene.children.filter( ( child: any ) => child.isMesh && child.__removed === undefined ));
+            return extracted_meshes; // [ [mesh0, mesh1, mesh2, ...], [mesh0, mesh1, mesh2, ...], [mesh0, mesh1, mesh2, ...], ... ]
+        }
+    };
+    AddAllMeshesToPageDataOf( 'test_page' );
+    */
+
+/*
+return oldPages.map( ( oldPage ) => {
+    return {
+        ...oldPage,
+        models: oldPage.models.map( (model: any, i: number) => {
+            return {
+                ...model,
+                meshes: extracted_meshes[i]
+            }
+        })
+    }
+})
+*/
+
+
+
+
+/*
+function Init() {
+    // Will set App global state data by loading all models, extracting all meshes, and creating all animations.
+
+}
+*/
