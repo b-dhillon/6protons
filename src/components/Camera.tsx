29c390616e543f6a508ea604a7bd7977fc954ca0
B. Dhillon <88413313+b-dhillon@users.noreply.github.com>
2023-10-16 18:06:05 -0700

    refactor: camera animation mixers

--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -18,7 +18,6 @@ import { PerspectiveCamera, useHelper } from '@react-three/drei';
 
 /** reversing navigation to-do:
  * 
- * 2. Try to clean up the mixers. -- Try 1 mixer again, but this time without the weird [section-1]
  * 3. Prevent ability to animate camera past maxSection and minSection
  * 4. Re-factor and clean up code
  * 5. Add checks for isRunning so that user cant click increment again if animation is currently still running.
@@ -32,43 +31,35 @@ export function Camera( { initializedPage, section }: any ): JSX.Element {
   const ref = useRef();
   const prevSection = useRef();
   const maxSection = initializedPage.maxSection
-  // const [ mixer, setMixer ] = useState();
+  const [ mixer, setMixer ] = useState();
 
 
   // creates all AnimationActions via looping camera.animationClips[]
   useEffect(() => {
-
-    // creating only 1 mixer:
-    /**
-      if( !mixer ) {
-         setMixer(new THREE.AnimationMixer(ref.current));
-        console.log("Animation mixer created");
-      } 
-      if (mixer) {
-     * 
-     */
-
+    // if no mixer, create mixer
+    if( !mixer ) {
+      setMixer(new THREE.AnimationMixer(ref.current));
+      console.log("Animation mixer created");
+    } 
+    // if mixer, animations are ready to be created from mixer:
+    else {
       // maps over animationClips and creates an AnimationAction for each AnimationClip
       function createAnimations( ref: any, animationClips: [][] ): AnimationAction[] {
-  
         function createAnimation( clip: THREE.AnimationClip ): THREE.AnimationAction {
-          const mixer = new THREE.AnimationMixer(ref);
+          // mixer = new THREE.AnimationMixer(ref);
           const animation = mixer.clipAction(clip);
           animation.loop = THREE.LoopOnce;
           animation.clampWhenFinished = true;
           return animation;
         }; 
-  
         return animationClips.map((animationClip: []) => createAnimation(animationClip[0]) ); 
         // Why is index hard-coded 0?? --> because theres only one animation per section
         // I believe I initially built it to be explandable to multiple animations per section.
         // But this should be re-factored to an object, like what we did with the model's multiple animations.
-  
       };
       setAnimations( createAnimations(ref.current, camera.animationClips) );
-      
-    // }
-  }, []);
+    }
+  }, [mixer]);
 
 
   // animationController --> Plays the AnimationAction based on section
@@ -78,87 +69,57 @@ export function Camera( { initializedPage, section }: any ): JSX.Element {
 
     function animationController() {
       if (animations.length) {
-        // if (section !== 1) 
-        // animations[section].play().warp(1, 0.01, 7.8); // .warp( 1.3, 0.01, 4.6 );
+        // ensure all animations are stopped before playing the next one:
+        animations.forEach( ( animation: AnimationAction ) => animation.stop() );
 
-
-        // This is needed for the first animation before the start-button is clicked.
+        // This is needed for the first animation, before the start-button is clicked.
+        // This should be re-factored however. Make the whole camera a stack. Start the stack at -1. 
+        // When lesson is loaded, move the stack from -1 to 0, triggering the first entrance animation as a forwards animation
+        // With that logic, you would theoretically not need this extra block.
         if( section === 0 ) {
           console.log("ORIGIN block!, currSection:", section);
-
           const backwardsNavigation: boolean = (prevSection.current - section) > 0;
           const forwards = !backwardsNavigation
-
           if (forwards) {
             animations[0].reset();
-            animations[0].timeScale = 1; // needed if warp? first argument in warp() sets the timeScale
-            
             animations[0].play().warp(1, 0.01, 7.8);
-          }
-          else {
+          } else {
             console.log("BACKWARDS!, currSection:", section);
             animations[section + 1].reset();
             animations[section + 1].time = camera.animationClips[0][0].duration
-            // animations[section + 1].timeScale = -1; // needed if warp? first argument in warp() sets the timeScale
-            
             animations[section + 1].play().warp(-1, 0.01, 7.8);
-          }
+          };
           prevSection.current = 0;
         };
 
         if (prevSection.current !== undefined && section !== 0) {
           console.log('prevSection', prevSection.current);
-
           // if delta negative, move up the stack:
           const forwardsNavigation: boolean = ( prevSection.current - section ) < 0;
           // if delta positive, move down the stack:
           const backwardsNavigation: boolean = ( prevSection.current - section ) > 0;
-
           if (forwardsNavigation) {
             console.log("FORWARDS!, currSection:", section);
             animations[section].reset();
-            // animations[section].timeScale = 1; // needed if warp? first argument in warp() sets the timeScale
-            
             animations[section].play().warp(1, 0.01, 7.8);
             prevSection.current = section;
           };
-
           if (backwardsNavigation) {
             console.log("BACKWARDS!, currSection:", section);
             animations[section + 1].reset();
             animations[section + 1].time = camera.animationClips[section][0].duration
-            // animations[section + 1].timeScale = -1; // needed if warp? first argument in warp() sets the timeScale
             animations[section + 1].play().warp(-1, 0.01, 7.8);
             prevSection.current = section;
           };
         };
       };
     };
-
   }, [animations, section]);
-
-
 
   // Updates animation via mixer
   useFrame((_, delta) => {
-    if (animations.length) {
-
-      // This needs to be cleaned up and written better. 
-      // Im pretty sure only 1 mixer is needed.
-      // mixer.update(delta)
-
-
-      // For the entrance animation
-      if (section===0) animations[0]._mixer.update(delta)
-      // For the forwards and reverse animations 
-      animations[section]._mixer.update(delta)
-      // if (section!==0)animations[section+1]._mixer.update(delta)
-      if( section < maxSection ) animations[section+1]._mixer.update(delta)
-    };
-    // animations[1]._mixer.update(delta);
+    if (animations.length && mixer ) mixer.update(delta)
   });
-
-
 
   // Setting the scene's camera. There are two. Perspective and Development.
   useHelper( ref, THREE.CameraHelper );
@@ -166,7 +127,6 @@ export function Camera( { initializedPage, section }: any ): JSX.Element {
    const set = useThree((state) => state.set);
    useEffect(() => set({ camera: ref.current }));
   */
-
 
   return (
     <>
@@ -199,8 +159,33 @@ export function Camera( { initializedPage, section }: any ): JSX.Element {
 
 
 
-// import { CameraHelper } from "three"; <-- Not needed if THREE is already imported with *
 
+
+
+/** Old way of doing animation mixers --> multiple mixers, 1 for each action
+  // For the entrance animation
+  if (section===0) animations[0]._mixer.update(delta)
+  // For the forwards and reverse animations 
+  animations[section]._mixer.update(delta)
+  // if (section!==0)animations[section+1]._mixer.update(delta)
+  if( section < maxSection ) animations[section+1]._mixer.update(delta)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { CameraHelper } from "three"; <-- Not needed if THREE is already imported with *
 
 // function SetCamera(cam): void {
 //   set({ camera: cam });
